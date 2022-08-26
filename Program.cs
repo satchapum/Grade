732c54bc590e10eda7373ca544c7d71b0bbe601d
Brazil <satchapum.mail@mail.kmutt.ac.th>
2022-08-26 10:40:40 +0700

    init project 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class program
+{
+    static void Main(string[] args)
+    {
+         Console.WriteLine("Hello, World!");
+    }
+}
+
